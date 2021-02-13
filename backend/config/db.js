600b3e042ae90b2ac221ca19809a223bf2780f55
Rafael Liechti <rafi.li@hotmail.com>
2021-02-13 13:48:14 +0100

    init

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import '../model/userModel.js';
+
+export default async function connectDB(database) {
+    try {
+        const con = await mongoose.connect(database, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log(`Database connected : ${con.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
